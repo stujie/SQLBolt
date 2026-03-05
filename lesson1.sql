4824d590dcc8bf2408bbb86e98ffc76424d91172
Stutie <stutiebanerjie@gmail.com>
2026-03-04 23:15:54 -0500

    Update lesson1.sql

--- lesson1.sql
+++ lesson1.sql
@@ -1,9 +1,12 @@
-SELECT *
-FROM tutorial.billboard_top_100_year_end
-WHERE year = 2013
-AND ("group_name" ILIKE '%macklemore%' 
-    OR "group_name" ILIKE '%timberlake%')
-
-
+-- Find the title of each film
+SELECT title FROM movies;
+-- Find the director of each film
+SELECT director FROM movies;
+-- Find the title and director of each film
+SELECT title, director FROM movies;
+-- Find the title and year of each film
+SELECT title, year FROM movies;
+-- Find all the information about each film
+SELECT * FROM movies;
 
     
